96b6cc171329bc64ff6017b348555e2e3b292a71
Paige Stephenson <paigestephenson29@gmail.com>
2023-03-13 09:18:19 -0600

    Initial Commit

---
+++ App Event Count/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Event Count
+//
+//  Created by Paige Stephenson on 3/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
